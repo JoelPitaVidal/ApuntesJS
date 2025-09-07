cf1e24b959a92af171cf0352b0ff4d315289fef2
JoelPitaVidal <jpitavidal@daielcastelao.org>
2025-09-07 17:57:09 +0200

    Recursivity, Closures and Spread functions

--- Intermediate/01-advanced-functions.js
+++ Intermediate/01-advanced-functions.js
@@ -58,3 +58,51 @@ handler.arrowGreeting(); // Hello undefined
 (() => {
     console.log("Arrow IIFE");
 })(); // This arrow function runs immediately upon definition.
+
+// Rest Parameters
+// Rest parameters allow a function to accept an indefinite number of arguments as an array.
+function sum(...numbers) {
+    let result = 0;
+    for (let number of numbers) {
+        result += number;
+    }
+    return result;
+}
+
+console.log(sum(1, 2, 3, 4, 5)); // 15
+console.log(sum(10, 15)); // 25
+
+// Spread Operator
+// The spread operator (`...`) allows an iterable (like an array) 
+// to be expanded in places where zero or more arguments (for function calls) or elements (for array literals) are expected.
+const arr1 = [1, 2, 3];
+function sumThree(a, b, c) {
+    return a + b + c;
+}
+
+console.log(sumThree(1, 2, 3)); // 6
+console.log(sumThree(...arr1)); // 6
+
+// Closures
+// A closure is a function that retains access to its lexical scope, even when the function is executed outside that scope.
+function createCounter() {
+    let count = 0; // Private variable
+    return function() {
+        count ++
+        console.log(count);
+    }
+}
+const counter = createCounter();
+counter(); // 1
+counter(); // 2
+
+// Recursivity
+// A recursive function is a function that calls itself in order to solve a problem.
+function factorial(n) {
+    if (n <= 1) return 1; // Base case
+    return n * factorial(n - 1); // Recursive case
+}
+
+console.log(factorial(5)); // 120
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
